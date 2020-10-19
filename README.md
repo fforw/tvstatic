5919d91baf8696b805bc5636d73e3c3268f0f5de
Sven Helmberger <fforw@gmx.de>
2020-10-19 15:02:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Starter
+
+Simple JS build starter for simple, client-only demos.
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import domready from "domready"
+// noinspection ES6UnusedImports
+import STYLE from "./style.css"
+
+const PHI = (1 + Math.sqrt(5)) / 2;
+const TAU = Math.PI * 2;
+const DEG2RAD_FACTOR = TAU / 360;
+
+const config = {
+    width: 0,
+    height: 0
+};
+
+let ctx, canvas;
+
+domready(
+    () => {
+
+        canvas = document.getElementById("screen");
+        ctx = canvas.getContext("2d");
+
+        const width = (window.innerWidth) | 0;
+        const height = (window.innerHeight) | 0;
+
+        config.width = width;
+        config.height = height;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0,0, width, height);
+    }
+);
